a91f30b68f2ba89e541db23e1ce2176c1dd7a93d
caoxin <caoxin1988s@gmail.com>
2018-12-07 00:16:46 +0800

    add bfs algothrim for graph with python
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

--- python/graph/Graph.py
+++ python/graph/Graph.py
@@ -18,12 +18,34 @@ class Graph:
     def get_graph(self):
         return self.adj
 
+    def _print(self, prev : dict, s, t):
+        if prev[t] == s:
+            print('path: ', s)
+            return
+        
+        self._print(prev, s, prev[t])
+        print('path: ', t)
+
     def bfs(self, s, t):
-        pass
+        visited = set()
+        prev = {}
+        queue = collections.deque([s])
+
+        while len(queue):
+            element = queue.popleft()
+            for item in self.adj[element]:
+
+                if item == t:
+                    prev[item] = element
+                    self._print(prev, s, t)
+                    return
+                elif item not in visited:
+                    prev[item] = element
+                    visited.add(item)
+                    queue.append(item)
 
 graph = Graph()
 graph.insert(1, 2)
-graph.insert(1, 3)
 graph.insert(1, 4)
 graph.insert(1, 5)
 graph.insert(2, 3)
@@ -31,6 +53,8 @@ graph.insert(2, 4)
 graph.insert(2, 5)
 graph.insert(3, 4)
 graph.insert(3, 5)
+graph.insert(3, 6)
 graph.insert(4, 5)
 
 print(graph.get_graph())
+graph.bfs(1, 6)
